db0977da636a4fa69ab4b5874c048c832c2a91e1
Bela Jash <abelxotessema@gmail.com>
2024-08-10 16:48:17 +0300

    Replacing Callbacks with Promises.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ getUser(1, fetchRepositories);
 console.log(`After`);
 
 function fetchRepositories(user) {
-  getRepositories(user.gitHubUsername, fetchCommits)
+  getRepositories(user.gitHubUsername, fetchCommits);
 }
 
 function fetchCommits(repositories) {
@@ -14,21 +14,29 @@ function displayCommits(commits) {
   console.log(`Commits:`, commits);
 }
 
-function getUser(id, callback) {
-  setTimeout(() => {
+function getUser(id) {
+  return new Promise((resolve, reject) => {
     console.log(`Reading a user from a database…`);
-    callback({id: id, gitHubUsername: `Béla Jash`});
-  }, 2_000);
+    setTimeout(() => {
+      resolve({id: id, gitHubUsername: `Béla Jash`});
+    }, 2_000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    callback([`repo 1`, `repo 2`, `repo 3`]);
-  }, 2_000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching repositories of ${username}`);
+    setTimeout(() => {
+      resolve([`repo 1`, `repo 2`, `repo 3`]);
+    }, 2_000);
+  });
 }
 
-function getCommits(repository, callback) {
-  setTimeout(() => {
-    callback([`commit 1`, `commit 2`, `commit 3`]);
-  }, 2_000);
+function getCommits(repository) {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching commits of ${repository}`);
+    setTimeout(() => {
+      resolve([`commit 1`, `commit 2`, `commit 3`]);
+    }, 2_000);
+  });
 }
