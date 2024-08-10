973ed3be75d79f2f8691057bec058ecf47de8321
Bela Jash <abelxotessema@gmail.com>
2024-08-10 16:00:24 +0300

    Callbacks.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 console.log(`Before`);
-const user = getUser(1);
-console.log(user);
+getUser(1, function(user) {
+  console.log(`User:`, user);
+  getRepositories(user.gitHubUsername, (repositories) => {
+    console.log(`Repositories of ${user.gitHubUsername}:`, repositories);
+  })
+});
 console.log(`After`);
 
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log(`Reading a user from a database…`);
-    return {id: id, githubUsername: `Béla Jash`};
+    callback({id: id, gitHubUsername: `Béla Jash`});
+  }, 2_000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    callback([`repo 1`, `repo 2`, `repo 3`]);
   }, 2_000);
 }
